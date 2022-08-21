b0655ad631c0a6abb0685f9d63958c520d71dc7b
Christian <christianpompeu@gmail.com>
2022-08-21 00:09:03 -0300

    creating repo list

---
+++ src/pages/repositories/index.tsx
@@ -0,0 +1,17 @@
+import { Heading } from "@chakra-ui/react";
+import Container from "../../components/Container";
+import { Hero } from "../../components/Hero";
+import { Main } from "../../components/Main";
+
+function Repositories() {
+  return (
+    <Container>
+      <Hero title="Lsita de Repositorios" />
+      <Main>
+        
+      </Main>
+    </Container>
+  );
+}
+
+export default Repositories;
