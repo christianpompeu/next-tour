65fac233bf6a8d16bfdad8662f2c83e8d75bef9d
Christian <christianpompeu@gmail.com>
2022-08-25 00:23:06 -0300

    otimizando o retorno

--- src/pages/posts/[name].tsx
+++ src/pages/posts/[name].tsx
@@ -1,6 +1,7 @@
 import Container from "../../components/Container";
 import { Hero } from "../../components/Hero";
 import axios from "axios";
+import { Image } from "@chakra-ui/react";
 
 export async function getStaticPaths() {
   const paths = [
@@ -16,19 +17,17 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
   const name = context.params.name;
-  var data = {};
   const repository = await axios
     .get(`https://api.github.com/repos/christianpompeu/${name}`)
     .then((response) => {
-      data = response.data;
+      return response.data;
     });
   return {
     props: {
-      id: data.id,
-      node_id: data.node_id,
-      full_name: data.full_name,
-      avatar_url: data.owner.avatar_url,
-      full_name: data.full_name,
+      id: repository.id,
+      node_id: repository.node_id,
+      full_name: repository.full_name,
+      avatar_url: repository.owner.avatar_url,
     }, // will be passed to the page component as props
   };
 }
@@ -38,6 +37,7 @@ function Post(props) {
   return (
     <Container>
       <Hero title={`Id do post: ${props.id}`} />
+      <Image boxSize="100px" borderRadius="full" src={props.avatar_url} />
     </Container>
   );
 }
